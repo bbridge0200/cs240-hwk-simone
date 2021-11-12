4c4fa89ac41275e55e63448120d3df0c76fb2cae
Beatrice Bridge <beatricembridge@gmail.com>
2021-11-12 09:45:56 -0800

    Fixed audio bug, successfully implemented pushButton, playSequence, playWelcome

--- simone.js
+++ simone.js
@@ -5,14 +5,6 @@ let R = document.querySelector("#redSq");
 let Y = document.querySelector("#yellowSq");
 let G = document.querySelector("#greenSq");
 
-var audioB = new Audio("sounds/blue.wav");
-var audioR = new Audio("sounds/red.wav");
-var audioY = new Audio("sounds/yellow.wav");
-var audioG = new Audio("sounds/green.wav");
-var audioLose = new Audio("sounds/lose.wav");
-var audioNextRound = new Audio("sounds/nextRound.wav");
-var audioWin = new Audio("sounds/win.mp3");
-var audioWrong = new Audio("sounds/wrong.wav");
 //1. get it to play two buttons with delay
 //2. get it to play practice sequence
 //3. get it to play first elemtne, 1,2 elements
@@ -20,12 +12,10 @@ var audioWrong = new Audio("sounds/wrong.wav");
 
 //async get Pattern...,.., cmd=start
 //if color, change colors, with delay
-//new audio
-// change color back
 //for each of the rounds play that rounds # of patterns, if click pattern matches then go to next element, else
 //exit, else if its last round and they are right then.... winning screen....
 function getSequence(numRounds) {
-  return ["B", "R", "G", "R", "G"];
+  return ["B", "R", "G"];
 }
 function playGame(numRounds) {
   //let openingSequence = get sequence(10);
@@ -76,49 +66,64 @@ function darkenColor(node) {
 }
 function playAudio(node) {
   if (node == B) {
+    let audioB = new Audio("sounds/blue.wav");
     audioB.play();
   }
   if (node == G) {
+    let audioG = new Audio("sounds/green.wav");
     audioG.play();
   }
   if (node == R) {
+    let audioR = new Audio("sounds/red.wav");
     audioR.play();
   }
   if (node == Y) {
+    let audioY = new Audio("sounds/yellow.wav");
     audioY.play();
   }
+  /* var audioB = new Audio("sounds/blue.wav");
+  var audioR = new Audio("sounds/red.wav");
+  var audioY = new Audio("sounds/yellow.wav");
+  var audioG = new Audio("sounds/green.wav");
+  var audioLose = new Audio("sounds/lose.wav");
+  var audioNextRound = new Audio("sounds/nextRound.wav");
+  var audioWin = new Audio("sounds/win.mp3");
+  var audioWrong = new Audio("sounds/wrong.wav"); */
 }
-async function buttonPress(node, milliseconds) {
-  //const result = await playAudio(node);
 
+async function buttonPress(node, milliseconds) {
+  //make new audio everytime i play it.
+  await new Promise((r) => setTimeout(r, milliseconds));
   lightenColor(node);
   playAudio(node);
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve();
-    }, milliseconds)
-  );
+  await new Promise((r) => setTimeout(r, milliseconds));
   darkenColor(node);
 }
-//function whiteBorder
 function playWelcome(sequenceArray) {
   playSequence(sequenceArray, 0, 120);
-  //sequenceArray.foreach(playOneButton());
 }
 async function playSequence(sequenceArray, delay1, delay2) {
   //for each letter play sound!
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve(); // do nothing after waiting 100 ms, just alert the calling thread
-    }, delay1)
-  );
+  await new Promise((r) => setTimeout(r, delay1));
+
   for (let i = 0; i < sequenceArray.length; i++) {
-    try {
-      let result = await buttonPress(B, delay2);
-      console.log(result); // yay, Promise resolved!
-    } catch (error) {
-      console.log(error); // boo, Promise rejected!
-    }
+    let node = convertToNode(sequenceArray[i]);
+    await buttonPress(node, delay2);
+    console.log(sequenceArray[i]);
+  }
+}
+function convertToNode(node) {
+  if (node == "B") {
+    return B;
+  }
+  if (node == "G") {
+    return G;
+  }
+  if (node == "R") {
+    return R;
+  }
+  if (node == "Y") {
+    return Y;
   }
 }
 
