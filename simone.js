10a1c62964ec3ad756f360e3571f784dd5ae6447
Beatrice Bridge <beatricembridge@gmail.com>
2021-11-16 10:52:02 -0800

    comments, reduced repetition in button listening events with a for each looop

--- simone.js
+++ simone.js
@@ -1,9 +1,13 @@
+/* SIMONE HOMEWORK CSCI 240
+11/15/21
+AUTHOR: BEE BRIDGE */
 let roundsAmt = document.querySelector("#rounds");
 let playButton = document.querySelector("#play");
 let B = document.querySelector("#blueSq");
 let R = document.querySelector("#redSq");
 let Y = document.querySelector("#yellowSq");
 let G = document.querySelector("#greenSq");
+let buttons = [B, R, Y, G];
 let text = document.querySelector("#status");
 let body = document.querySelector("body");
 let isPlaying = false;
@@ -11,7 +15,10 @@ let spotInSequence = 0;
 let currSequence = [];
 let currRound = 0;
 let welcomeSequence = [];
-
+/*
+lightens the color of the given node to
+its lighter color
+*/
 function lightenColor(node) {
   if (node == B) {
     node.style.backgroundColor = "lightblue";
@@ -30,6 +37,10 @@ function lightenColor(node) {
   }
   node.style.border = "";
 }
+/*
+darkens the color of the node
+or screen provided
+*/
 function darkenColor(node) {
   if (node == B) {
     node.style.backgroundColor = "#0000bb";
@@ -48,6 +59,10 @@ function darkenColor(node) {
   }
   node.style.border = "";
 }
+/*
+plays the audio for the provided node
+or text
+*/
 function playAudio(node) {
   if (node == B) {
     let audioB = new Audio("sounds/blue.wav");
@@ -82,7 +97,10 @@ function playAudio(node) {
     audioWR.play();
   }
 }
-
+/*
+plays the button sound and changes the colors
+with the provided delay
+*/
 async function buttonPress(node, milliseconds) {
   await new Promise((r) => setTimeout(r, milliseconds));
   lightenColor(node);
@@ -90,9 +108,15 @@ async function buttonPress(node, milliseconds) {
   await new Promise((r) => setTimeout(r, milliseconds));
   darkenColor(node);
 }
+/*
+plays the welcome sequence, with appropriate delay
+*/
 function playWelcome(sequenceArray) {
   playSequence(sequenceArray, 0, 120);
 }
+/*
+plays the audio for each sound in a sequence
+*/
 async function playSequence(sequenceArray, delay1, delay2) {
   //for each letter play sound!
   await new Promise((r) => setTimeout(r, delay1));
@@ -102,6 +126,10 @@ async function playSequence(sequenceArray, delay1, delay2) {
     await buttonPress(node, delay2);
   }
 }
+/*
+Converts a string to a button object 
+created above
+*/
 function convertToNode(node) {
   if (node == "B") {
     return B;
@@ -116,17 +144,19 @@ function convertToNode(node) {
     return Y;
   }
 }
-
+/*
+EVENT LISTENER FOR THE PLAY BUTTON. STARTS GAME WITH SPECIFIED NUMBER OF ROUNDS. 
+DEFAULTS TO 10 ROUNDS ON INVALID INPUT.Uses API to get the sequence for welcome 
+and for the game given the number of rounds
+*/
 playButton.addEventListener("click", async function (evt) {
   //reset screen
   clearGame();
   currRound = 1;
   isPlaying = true;
-  //let welcome = getSequence("welcome");
   const axios = require("axios");
 
   try {
-    // get a joke
     const hdrs = {
       headers: { Accept: "application/json" },
     };
@@ -150,6 +180,10 @@ playButton.addEventListener("click", async function (evt) {
     return "error!";
   }
 });
+/*
+Checks a click during game play to see if it is the
+correct or incorrect move in the game. try catch around axios call. 
+*/
 async function checkClick(node) {
   if (isPlaying) {
     if (node === convertToNode(currSequence[spotInSequence])) {
@@ -186,16 +220,26 @@ async function checkClick(node) {
     }
   }
 }
+/*
+Displays lose message, plays audio, and changes the screen color.
+*/
 function lose() {
   text.innerHTML = "Incorrect! You lose!";
   lightenColor(body);
   playAudio("lose");
 }
+/*
+Displays win message, plays audio, and changes the screen color.
+*/
 function win() {
   text.innerHTML = "Yay you win!";
   darkenColor(body);
   playAudio("win");
 }
+/*
+Resets the screen and text to the 
+original when a game is finished. 
+*/
 function clearGame() {
   isPlaying = false;
   spotInSequence = 0;
@@ -204,61 +248,23 @@ function clearGame() {
   text.innerHTML = "";
   body.style.backgroundColor = "black";
 }
-B.addEventListener("mouseover", function (evt) {
-  B.style.border = "solid #eeeeee .5px";
-});
-B.addEventListener("mouseout", function (evt) {
-  darkenColor(B);
-});
-B.addEventListener("mousedown", function (evt) {
-  lightenColor(B);
-});
-B.addEventListener("mouseup", function (evt) {
-  //TODO MULTIPLE CLICKS IN A ROW
-  darkenColor(B);
-  playAudio(B);
-  checkClick(B);
-});
-
-R.addEventListener("mouseover", function (evt) {
-  R.style.border = "solid #eeeeee .5px";
-});
-R.addEventListener("mouseout", function (evt) {
-  darkenColor(R);
-});
-R.addEventListener("mousedown", function (evt) {
-  lightenColor(R);
-});
-R.addEventListener("mouseup", function (evt) {
-  darkenColor(R);
-  playAudio(R);
-  checkClick(R);
-});
-G.addEventListener("mouseover", function (evt) {
-  G.style.border = "solid #eeeeee .5px";
-});
-G.addEventListener("mouseout", function (evt) {
-  darkenColor(G);
-});
-G.addEventListener("mousedown", function (evt) {
-  lightenColor(G);
-});
-G.addEventListener("mouseup", function (evt) {
-  darkenColor(G);
-  playAudio(G);
-  checkClick(G);
-});
-Y.addEventListener("mouseover", function (evt) {
-  Y.style.border = "solid #eeeeee .5px";
-});
-Y.addEventListener("mouseout", function (evt) {
-  darkenColor(Y);
-});
-Y.addEventListener("mousedown", function (evt) {
-  lightenColor(Y);
-});
-Y.addEventListener("mouseup", function (evt) {
-  darkenColor(Y);
-  playAudio(Y);
-  checkClick(Y);
+/*
+for each button: highlight with user interaction
+check button clicks against current game state
+*/
+buttons.forEach((item) => {
+  item.addEventListener("mouseover", function (evt) {
+    item.style.border = "solid #eeeeee .5px";
+  });
+  item.addEventListener("mouseout", function (evt) {
+    darkenColor(item);
+  });
+  item.addEventListener("mousedown", function (evt) {
+    lightenColor(item);
+  });
+  item.addEventListener("mouseup", function (evt) {
+    darkenColor(item);
+    playAudio(item);
+    checkClick(item);
+  });
 });
